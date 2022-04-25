4f2053f68726d9ea93e0a702aeeed5d3ffbf5fe2
Ivan Kostyuk <ivkos@freya01.bc.rzg.mpg.de>
2022-04-25 17:26:13 +0200

    Changed the level0 builder to the SFR in gas particles based approach

--- df_level0.py
+++ df_level0.py
@@ -11,7 +11,7 @@ from pyTNG.cosmology import TNGcosmo
 
 
 def get_sim():
-    basepath = "/virgo/simulations/IllustrisTNG/"
+    basepath = '/virgotng/universe/IllustrisTNG/'
     sim_name = 'L35n2160TNG'
     sim = _data_interface.TNG50Simulation(os.path.join(basepath, sim_name))
     sim_path = os.path.join(basepath, sim_name, 'output')
@@ -115,59 +115,118 @@ def calculate_batched_quants(stars_arr, specFac):
         del batch['spectra']
     return ion_lum, luminosity
 
+def get_star_forming_gas(gas):
+    star_forming_gas = {}
+    star_forming = gas['StarFormationRate']>1e-9
+    star_forming_gas['SFR'] = gas['StarFormationRate'][star_forming]
+    star_forming_gas['Centers'] = gas['Coordinates'][star_forming]
+    star_forming_gas['Densities'] = gas['Density'][star_forming]
+    star_forming_gas['Z'] = gas['GFM_Metallicity'][star_forming]
+    star_forming_gas['Masses'] = gas['Masses'][star_forming]
+    star_forming_gas['count'] = np.sum(star_forming)
+    return star_forming_gas
 
-def add_lum_ion(df, sim_path, snap_num, z, specFac):
+
+def get_radius_vol(gas):
+    gas['Radii'] = ((3/(4*np.pi)*gas['Masses']/gas['Densities']))**(1/3)
+    gas['Volumes'] = gas['Masses']/gas['Densities']
+    return gas
+
+
+def get_stellar_dist_gas(stars, gas_coord, gas_rad):
+    rel_pos = stars['Coordinates']-gas_coord
+    dist = np.sqrt(np.sum(np.square(rel_pos), axis=1))
+    rel_dist = dist/gas_rad
+    stars['rel_dist'] = rel_dist
+    return
+
+
+def select_stars_in_gas(stars, gas):
+    relevant_stars = {}
+    for key in stars.keys():
+        if key == 'count':
+            relevant_stars[key] = 0
+        elif key == 'Redshift':
+            relevant_stars[key] = stars[key]
+        else:
+            relevant_stars[key] = []
+    for i, center in enumerate(gas['Centers']):
+        get_stellar_dist_gas(stars, center, gas['Radii'][i])
+        new_stars = stars['rel_dist']<1
+        for key in stars.keys():
+            if key not in {'count', 'Redshift', 'rel_dist'}:
+                relevant_stars[key].extend(stars[key][new_stars])
+                stars[key] = np.delete(stars[key], new_stars, axis=0)
+        relevant_stars['count'] += np.sum(new_stars)
+    for key in stars.keys():
+        if key not in {'count', 'Redshift', 'rel_dist'}:
+            relevant_stars[key] = np.array(relevant_stars[key])
+    return relevant_stars
+
+
+def add_quantities(df, sim_path, snap_num, z, specFac):
     luminosities = []
     ion_lums = []
-    df[('ion_lum', 0)] = np.nan
+    # df[('ion_lum', 0)] = np.nan
     for idx in df.index:
         stars = il.snapshot.loadSubhalo(sim_path, snap_num, idx, 'stars')
         utils.dropWindParticles(stars)
         stars['Redshift'] = z
-        stars['ages'] = spectra._computeAgeFromFormationTime(stars['Redshift'], stars['GFM_StellarFormationTime'])
-        get_stellar_dist(stars, df, idx)
-        idces = stars['rel_dist'] < 1
-        utils._keepPartsByIdx(stars, idces)
+        gas = il.snapshot.loadSubhalo(sim_path, snap_num, idx, 'gas')
+        relevant_gas = get_star_forming_gas(gas)
+        get_radius_vol(relevant_gas)
+        relevant_stars = select_stars_in_gas(stars, relevant_gas)
+        del stars
+        relevant_stars['ages'] = spectra._computeAgeFromFormationTime(relevant_stars['Redshift'], relevant_stars['GFM_StellarFormationTime'])
+        # get_stellar_dist(stars, df, idx)
+        # idces = stars['rel_dist'] < 1
+        # utils._keepPartsByIdx(stars, idces)
+
         # If no stars are left after filtering remove this halo from the df
-        if stars['count'] == 0:
+        if relevant_stars['count'] == 0:
             df.drop(index=idx, inplace=True)
             continue
 
-        if stars['count'] < 10000:
+        if relevant_stars['count'] < 10000:
             try:
-                specFac.computeStellarSpectra(stars, Q_0=True)
-                ion_lum = stars['Q_0'].sum()
-                summed_spectrum = stars['spectra'].sum(axis=0)
-                luminosity = integ.simps(summed_spectrum, stars['lambda'])
+                specFac.computeStellarSpectra(relevant_stars, Q_0=True)
+                ion_lum = relevant_stars['Q_0'].sum()
+                summed_spectrum = relevant_stars['spectra'].sum(axis=0)
+                luminosity = integ.simps(summed_spectrum, relevant_stars['lambda'])
             except:
                 print(f'An error occured in halo {idx}')
                 ion_lum = np.nan
                 luminosity = np.nan
         else:
             try:
-                star_arr = star_batches(stars, batchsize=10000)
+                star_arr = star_batches(relevant_stars, batchsize=10000)
                 ion_lum, luminosity = calculate_batched_quants(star_arr, specFac)
                 del star_arr
             except:
                 print(f'An error occured in halo {idx}')
                 ion_lum = np.nan
                 luminosity = np.nan
-        del stars
+
         luminosities.append(luminosity)
         ion_lums.append(ion_lum)
     df[('luminosity', 0)] = luminosities
     df[('ion_lum', 0)] = ion_lums
+    df[('GasMass', 0)] = np.sum(relevant_gas['Masses'])
+    df[('SFR', 0)] = np.sum(relevant_gas['SFR'])
+    df[('StarMass', 0)] = np.sum(relevant_stars['Masses'])
+    df[('Volume', 0)] = np.sum(relevant_gas['Volumes'])
+    df[('Z', 0)] = np.sum(relevant_gas['Z']*relevant_gas['Masses'])/np.sum(relevant_gas['Masses'])
     return
 
 
 def build_new_df(df, name, z, h):
     new_df = pd.DataFrame()
     new_df['HaloMass'] = df[('SubhaloMassInRad', 0)]
-    new_df['GasMass'] = df[('SubhaloMassInRadType', 0)]
-    new_df['StarMass'] = df[('SubhaloMassInRadType', 4)]
-    new_df['SFR'] = df[('SubhaloSFRinRad', 0)]
+    new_df['GasMass'] = df[('GasMass', 0)]
+    new_df['StarMass'] = df[('StarMass', 0)]
+    new_df['SFR'] = df[('SFR', 0)]
     new_df['com_radius'] = 2*df[('SubhaloHalfmassRadType', 4)]
-    new_df['Z'] = df[('SubhaloGasMetallicity', 0)]
+    new_df['Z'] = df[('Z', 0)]
     new_df['ion_lum'] = df[('ion_lum', 0)]
     new_df['luminosity'] = df[('luminosity', 0)]
 
@@ -198,7 +257,7 @@ def build_lv0(snap_num, output_name, from_tng=True, df_path=None, save_tng_df=Fa
     z = sim.snap_cat[snap_num].header['Redshift']
     h = TNGcosmo.h
 
-    add_lum_ion(df, sim_path, snap_num, z, specFac)
+    add_quantities(df, sim_path, snap_num, z, specFac)
     save_df(df, 'for_testing')
 
     build_new_df(df, output_name, z, h)
