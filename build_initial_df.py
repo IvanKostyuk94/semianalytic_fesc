8e56960aa9dbea1685b1a8eb207a2170fca777ac
Ivan Kostyuk <ivkos@freya01.bc.rzg.mpg.de>
2022-07-23 12:52:07 +0200

    Added new function to build the initial dataframe from TNG data

---
+++ build_initial_df.py
@@ -0,0 +1,16 @@
+from utils import *
+import os
+
+
+def build_new_df(snap_num, save_name='reduced_df'):
+    sim, simpath = get_sim()
+    full_df = get_dataset_df(sim, snap_num=snap_num)
+    save_df = reduce_df(full_df)
+    base = '/ptmp/mpa/ivkos/semianalytic_fesc/sn013'
+    save_path = os.path.join(base, save_name+'.pickle')
+    save_df.to_pickle(save_path)
+    return
+
+if __name__ == '__main__':
+    snap_num = 13
+    build_new_df(snap_num)
