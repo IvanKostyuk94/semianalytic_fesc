25d2671558e1d593729d0bf23a7e6f632e11d438
Ivan Kostyuk <kostyuk@hotmail.de>
2022-08-17 14:15:39 +0200

    Function which adds computes the effective escape fraction of the parent halo

--- match_with_crash.py
+++ match_with_crash.py
@@ -9,3 +9,11 @@ def get_parent_halo_ids(df, snap_num):
     parent_halos = dataset["SubhaloGrNr"][np.array(df.index)]
     df["Parent"] = parent_halos
     return
+
+
+def calculate_halo_fesc(df):
+    fesc = df.groupby("Parent").apply(
+        lambda gr: np.sum(gr["f_esc_sf_r"] * gr["Ion_em_sf_r"])
+        / gr["Ion_em_sf_r"].sum()
+    )
+    return fesc
