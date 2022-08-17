63ed56046510da9e9c3d75ebd808a1f8d138a7c6
Ivan Kostyuk <kostyuk@hotmail.de>
2022-08-17 15:15:37 +0200

    Added a function which does all the steps above and creates a dataframe with the semianalytic results for the halos

--- match_with_crash.py
+++ match_with_crash.py
@@ -26,3 +26,25 @@ def get_crash_halos_z(z, df_name="full_esc_updated.pickle"):
     full_df = pd.read_pickle(path_to_sel_df)
     df_z = full_df[full_df.z == z]
     return df_z
+
+
+def add_semianalytic_fesc(df_z, fesc):
+    df_z["fesc_semi"] = fesc.loc[df_z.index]
+    return
+
+
+def get_z(snap_num):
+    z_values = {4: 10, 8: 8, 13: 6}
+    try:
+        return z_values[snap_num]
+    except KeyError:
+        raise KeyError("No Crash results are provided for this snapshot")
+
+
+def get_crash_df_with_semi(df, snap_num, df_name="full_esc_updated.pickle"):
+    get_parent_halo_ids(df, snap_num)
+    fesc = calculate_halo_fesc(df)
+    z = get_z(snap_num)
+    df_z = get_crash_halos_z(z, df_name=df_name)
+    add_semianalytic_fesc(df_z, fesc)
+    return df_z
