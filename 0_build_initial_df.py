052675025329399c2a13afd0640fae6dc1e2a00f
Ivan Kostyuk <kostyuk@hotmail.de>
2022-09-06 16:12:34 +0200

    Creates a new folder for the snap if it does not exists yet

--- 0_build_initial_df.py
+++ 0_build_initial_df.py
@@ -8,6 +8,9 @@ def build_new_df(snap_num, save_name="df", base="/ptmp/mpa/ivkos/semianalytic_fe
     save_df = reduce_df(full_df)
     snap = get_snap(snap_num)
     snap_path = os.path.join(base, snap)
+    if not os.path.exists(snap_path):
+        print(f"Creating directory '{snap_path}' as it does not exist yet.")
+        os.system(f"mkdir {snap_path}")
     save_path = os.path.join(snap_path, "0_" + save_name + ".pickle")
     save_df.to_pickle(save_path)
     return
