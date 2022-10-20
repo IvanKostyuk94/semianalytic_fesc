14065cea204365234e16dd2691774a3f8d8c7e36
Ivan Kostyuk <kostyuk@hotmail.de>
2022-10-20 15:03:32 +0200

    moved deprecated code to new folder

---
+++ deprecated/1_get_volume.py
@@ -0,0 +1,194 @@
+import pandas as pd
+import numpy as np
+import os
+import illustris_python as il
+import astropy.units as u
+from pyTNG.cosmology import TNGcosmo
+from utils import get_sim
+import pyTNG.utils as utils
+from utils import get_particle_dist
+from utils import get_redshift
+
+h = TNGcosmo.h
+
+
+def keepWindParticles(starAndWindParts):
+    try:
+        idces = starAndWindParts["GFM_StellarFormationTime"] < 0
+        utils._keepPartsByIdx(starAndWindParts, idces)
+    except KeyError as e:
+        if (
+            str(e) == "GFM_StellarFormationTime"
+            and starAndWindParts["count"] == 0
+        ):
+            pass
+        else:
+            raise
+    return
+
+
+def separate_wind_stars(starAndWindParts):
+    try:
+        idces_wind = starAndWindParts["GFM_StellarFormationTime"] < 0
+        idces_stars = starAndWindParts["GFM_StellarFormationTime"] > 0
+
+        newcount_wind = (idces_wind == True).sum()
+        newcount_stars = (idces_stars == True).sum()
+
+        wind = {}
+        stars = {}
+        for key, value in starAndWindParts.items():
+            try:
+                wind[key] = value[idces_wind]
+                stars[key] = value[idces_stars]
+            # for Python scalars
+            except TypeError as e:
+                if "not subscriptable" in str(e):
+                    pass
+                else:
+                    raise
+            # for numpy scalars
+            except IndexError as e:
+                if "invalid index to scalar variable" in str(e):
+                    pass
+                else:
+                    raise
+        if "count" in starAndWindParts:
+            wind["count"] = newcount_wind
+            stars["count"] = newcount_stars
+    except KeyError as e:
+        if (
+            str(e) == "GFM_StellarFormationTime"
+            and starAndWindParts["count"] == 0
+        ):
+            pass
+        else:
+            raise
+
+    return wind, stars
+
+
+def update_df(df, sim_path, snap_num, z):
+    counter = 0
+    volumes = []
+    masses = []
+    tot_star_masses = []
+
+    volumes_half_rad = []
+    masses_half_rad = []
+    tot_star_masses_half_rad = []
+
+    mass_to_g = (1 * u.Msun).to(u.g).value * 1e10 / h
+    volume_to_cm3 = ((1 * u.kpc).to(u.cm).value / h / (1 + z)) ** 3
+    dist_to_cm = (1 * u.kpc).to(u.cm).value / h / (1 + z)
+
+    # r is converted here to cm to be consistend with the use of get_particle_dist
+    df["r"] = df["r"] * dist_to_cm
+    for idx in df.index:
+        gas = il.snapshot.loadSubhalo(sim_path, snap_num, idx, "gas")
+        wind_stars = il.snapshot.loadSubhalo(sim_path, snap_num, idx, "stars")
+        wind, stars = separate_wind_stars(wind_stars)
+
+        get_particle_dist(gas, df, idx)
+        get_particle_dist(wind, df, idx)
+
+        gas_in_rad = gas["rel_dist"] < 1
+        wind_in_rad = wind["rel_dist"] < 1
+        if len(wind_in_rad) > 0:
+            wind_masses_rad = wind["Masses"][wind_in_rad]
+        else:
+            wind_masses_rad = 0
+        gas_masses = gas["Masses"][gas_in_rad]
+        gas_densities = gas["Density"][gas_in_rad]
+        gas_volumes = gas_masses / gas_densities
+        volumes.append(np.sum(gas_volumes))
+        masses.append(np.sum(gas_masses) + np.sum(wind_masses_rad))
+
+        gas_in_half_rad = gas["rel_dist"] < 0.5
+        wind_in_half_rad = wind["rel_dist"] < 0.5
+        gas_masses_half_rad = gas["Masses"][gas_in_half_rad]
+        if len(wind_in_half_rad) > 0:
+            wind_masses_half_rad = wind["Masses"][wind_in_half_rad]
+        else:
+            wind_masses_half_rad = 0
+        gas_densities_half_rad = gas["Density"][gas_in_half_rad]
+        gas_volumes_half_rad = gas_masses_half_rad / gas_densities_half_rad
+        volumes_half_rad.append(np.sum(gas_volumes_half_rad))
+        masses_half_rad.append(
+            np.sum(gas_masses_half_rad) + np.sum(wind_masses_half_rad)
+        )
+
+        get_particle_dist(stars, df, idx)
+        stars_in_rad = stars["rel_dist"] < 1
+        star_masses = np.sum(stars["Masses"][stars_in_rad])
+        tot_star_masses.append(star_masses)
+
+        stars_in_half_rad = stars["rel_dist"] < 0.5
+        star_masses_half_rad = np.sum(stars["Masses"][stars_in_half_rad])
+        tot_star_masses_half_rad.append(star_masses_half_rad)
+
+        if counter % 100 == 0:
+            print(f"{100*counter/len(df):.2f}% of all halos processed")
+        counter += 1
+    df["Masses_2r"] = np.array(masses) * mass_to_g
+    df["Volumes_2r"] = np.array(volumes) * volume_to_cm3
+    df["Star_masses_2r"] = np.array(tot_star_masses) * mass_to_g
+
+    df["Masses_r"] = np.array(masses_half_rad) * mass_to_g
+    df["Volumes_r"] = np.array(volumes_half_rad) * volume_to_cm3
+    df["Star_masses_r"] = np.array(tot_star_masses_half_rad) * mass_to_g
+    return
+
+
+def get_star_mass(df, sim_path, snap_num, z):
+    counter = 0
+    tot_star_masses = []
+    mass_to_g = (1 * u.Msun).to(u.kg).value * 1e10 / h
+    for idx in df.index:
+        stars = il.snapshot.loadSubhalo(sim_path, snap_num, idx, "stars")
+        get_particle_dist(stars, df, idx)
+        stars_in_rad = stars["rel_dist"] < 1
+        star_masses = stars["Masses"][stars_in_rad]
+        tot_star_masses.append(np.sum(star_masses))
+        if counter % 100 == 0:
+            print(f"{100*counter/len(df):.2f}% of all halos processed")
+        counter += 1
+    df[("star_masses", 0)] = np.array(tot_star_masses) * mass_to_g
+    return
+
+
+# Function which removes columns which are represented twice such as
+# star and gas mass which we calculate in get_volume to ensure that
+# everything is consistent and converts the stellar half mass rad to cm
+def reformat_df(df, z):
+    columns_to_drop = ["M_gas_r", "M_gas_2r", "M_star_r", "M_star_2r"]
+    df.drop(labels=columns_to_drop, inplace=True, axis=1)
+
+    df["Redshift"] = z
+
+    to_rename = {
+        "Masses_r": "M_gas_r",
+        "Masses_2r": "M_gas_2r",
+        "Volumes_r": "V_r",
+        "Volumes_2r": "V_2r",
+        "Star_masses_r": "M_star_r",
+        "Star_masses_2r": "M_star_2r",
+    }
+    df.rename(columns=to_rename, inplace=True)
+    return
+
+
+if __name__ == "__main__":
+    level_0_name = "1_test_df.pickle"
+    snap_num = 13
+    level_1_name = "1_test_df.pickle"
+    base = "/ptmp/mpa/ivkos/semianalytic_fesc/sn013"
+
+    df_path = os.path.join(base, level_0_name)
+    df = pd.read_pickle(df_path)
+    sim, sim_path = get_sim()
+    z = get_redshift(sim, snap_num)
+    update_df(df, sim_path, snap_num, z)
+    reformat_df(df, z)
+    save_path = os.path.join(base, level_1_name)
+    df.to_pickle(save_path)
---
+++ deprecated/2_add_sf_lum.py
@@ -0,0 +1,399 @@
+from ast import excepthandler
+import os
+import re
+import pandas as pd
+import numpy as np
+import illustris_python as il
+from pyTNG.spectra import StarSpectrumFactory
+from pyTNG import spectra
+from pyTNG.cosmology import TNGcosmo
+import scipy.integrate as integ
+import pyTNG.utils as utils
+from utils import get_stellar_dist_gas
+from utils import get_particle_dist
+from utils import get_redshift
+from utils import get_sim
+import astropy.units as u
+
+
+h = TNGcosmo.h
+
+
+def build_spec_fac():
+    bpp = [
+        "/ptmp/mpa/mglatzle/TNG_f_esc/BPASSv2.2.1_release-07-18-Tuatara/",
+        "2.2.1",
+        "chab300",
+        "bin",
+    ]
+    specFac = StarSpectrumFactory(bpp)
+    return specFac
+
+
+def get_stellar_dist(stars, df, index):
+    gal_center = np.array(
+        [
+            df.loc[index]["Halo_pos_x"],
+            df.loc[index]["Halo_pos_y"],
+            df.loc[index]["Halo_pos_z"],
+        ]
+    )
+    rel_pos = stars["Coordinates"] - gal_center
+    radius = df.loc[index]["r"] * 2
+    dist = np.sqrt(np.sum(np.square(rel_pos), axis=1))
+    rel_dist = dist / radius
+    stars["rel_dist"] = rel_dist
+    return
+
+
+def star_batches(stars, batchsize):
+    stars_arr = []
+    batches = stars["count"] // batchsize + 1
+    start = 0
+    end = batchsize
+    for i in range(batches):
+        star_batch = {}
+        star_batch["Redshift"] = stars["Redshift"]
+        if i + 1 < batches:
+            star_batch["count"] = batchsize
+            for key in stars.keys():
+                if key not in ["count", "Redshift"]:
+                    star_batch[key] = stars[key][start:end]
+            stars_arr.append(star_batch)
+            start = end
+            end += batchsize
+        else:
+            for key in stars.keys():
+                if key not in ["count", "Redshift"]:
+                    star_batch[key] = stars[key][start:]
+            star_batch["count"] = len(stars["ages"][start:])
+            stars_arr.append(star_batch)
+    return stars_arr
+
+
+def calculate_batched_quants(stars_arr, specFac):
+    ion_lum = 0
+    luminosity = 0
+    for batch in stars_arr:
+        specFac.computeStellarSpectra(batch, Q_0=True)
+        ion_lum += batch["Q_0"].sum()
+        summed_spectrum = batch["spectra"].sum(axis=0)
+        luminosity += integ.simps(summed_spectrum, batch["lambda"])
+        del batch["spectra"]
+    return ion_lum, luminosity
+
+
+def get_star_forming_gas(gas):
+    star_forming_gas = {}
+    star_forming = gas["StarFormationRate"] > 1e-9
+    star_forming_gas["SFR"] = gas["StarFormationRate"][star_forming]
+    star_forming_gas["Centers"] = gas["Coordinates"][star_forming]
+    star_forming_gas["Densities"] = gas["Density"][star_forming]
+    star_forming_gas["Coordinates"] = gas["Coordinates"][star_forming]
+    star_forming_gas["Z"] = gas["GFM_Metallicity"][star_forming]
+    star_forming_gas["Masses"] = gas["Masses"][star_forming]
+    star_forming_gas["count"] = np.sum(star_forming)
+    return star_forming_gas
+
+
+def select_stars_in_gas(stars, gas):
+    relevant_stars = {}
+    for key in stars.keys():
+        if key == "count":
+            relevant_stars[key] = 0
+        elif key == "Redshift":
+            relevant_stars[key] = stars[key]
+        else:
+            relevant_stars[key] = []
+    for i, center in enumerate(gas["Centers"]):
+        get_stellar_dist_gas(stars, center, gas["Radii"][i])
+        new_stars = stars["rel_dist"] < 1
+        if any(new_stars):
+            stars_to_delete = [element for element, x in enumerate(new_stars) if x]
+            for key in stars.keys():
+                if key not in {"count", "Redshift", "rel_dist"}:
+                    relevant_stars[key].extend(stars[key][new_stars])
+                    stars[key] = np.delete(stars[key], stars_to_delete, axis=0)
+        relevant_stars["count"] += np.sum(new_stars)
+    for key in stars.keys():
+        if key not in {"count", "Redshift", "rel_dist"}:
+            relevant_stars[key] = np.array(relevant_stars[key])
+    return relevant_stars
+
+
+def get_radius_vol(gas):
+    gas["Radii"] = ((3 / (4 * np.pi) * gas["Masses"] / gas["Densities"])) ** (1 / 3)
+    gas["Volumes"] = gas["Masses"] / gas["Densities"]
+    return gas
+
+
+def get_subset(parts, idces):
+    try:
+        newcount = (idces == True).sum()
+
+        subset = {}
+        for key, value in parts.items():
+            try:
+                subset[key] = value[idces]
+            # for Python scalars
+            except TypeError as e:
+                if "not subscriptable" in str(e):
+                    pass
+                else:
+                    raise
+            # for numpy scalars
+            except IndexError as e:
+                if "invalid index to scalar variable" in str(e):
+                    pass
+                else:
+                    raise
+        if "count" in parts:
+            subset["count"] = newcount
+        if "Redshift" in parts:
+            subset["Redshift"] = parts["Redshift"]
+    except KeyError as e:
+        if str(e) == "GFM_StellarFormationTime" and parts["count"] == 0:
+            pass
+        else:
+            raise
+
+    return subset
+
+
+def compute_small_gal_em(stars, specFac, idx):
+    try:
+        specFac.computeStellarSpectra(stars, Q_0=True)
+        ion_em = stars["Q_0"].sum()
+        summed_spectrum = stars["spectra"].sum(axis=0)
+        bol_lum = integ.simps(summed_spectrum, stars["lambda"])
+    except:
+        print(f"An error occured in halo {idx}")
+        ion_em = np.nan
+        bol_lum = np.nan
+    return ion_em, bol_lum
+
+
+def compute_large_gal_em(stars, specFac, idx):
+    try:
+        star_arr = star_batches(stars, batchsize=10000)
+        ion_em, bol_lum = calculate_batched_quants(star_arr, specFac)
+        del star_arr
+    except:
+        print(f"An error occured in halo {idx}")
+        ion_em = np.nan
+        bol_lum = np.nan
+    return ion_em, bol_lum
+
+
+# Function that adds all stars within half the stellar mass radius to the stars within
+# one stellar mass radius to safe the time of selecting the stars twice
+def merge_star_dict(star_dict, half_star_dict):
+    star_dict["count"]
+    try:
+        for key, value in star_dict.items():
+            if key not in {"count", "rel_dist", "Redshift"}:
+                star_dict[key] = np.append(star_dict[key], half_star_dict[key])
+
+        if "count" in star_dict:
+            star_dict["count"] += half_star_dict["count"]
+    except KeyError as e:
+        if str(e) == "GFM_StellarFormationTime" and ["count"] == 0:
+            pass
+        else:
+            raise
+    except:
+        print(key)
+        print(star_dict[key])
+        raise
+
+    return
+
+
+# Function which adds additional subhalo properties (luminosities,
+# ionizing luminosities, gas_masses, star_masses, sfrs, metallicities
+# and volumes) corresponding to only star forming gas cells
+def add_sf_quantities(df, sim_path, snap_num, z):
+
+    mass_to_g = (1 * u.Msun).to(u.g).value * 1e10 / h
+    volume_to_cm3 = ((1 * u.kpc).to(u.cm) / h / (1 + z)) ** 3
+    L_sun_to_erg_s = (1 * u.Lsun).to(u.erg / u.s)
+
+    bol_lums_2r = []
+    ion_ems_2r = []
+    gas_masses_2r = []
+    star_masses_2r = []
+    sfrs_2r = []
+    metallicities_2r = []
+    volumes_2r = []
+
+    bol_lums_r = []
+    ion_ems_r = []
+    gas_masses_r = []
+    star_masses_r = []
+    sfrs_r = []
+    metallicities_r = []
+    volumes_r = []
+
+    tot_bol_lums_2r = []
+    tot_ion_ems_2r = []
+
+    tot_bol_lums_r = []
+    tot_ion_ems_r = []
+
+    specFac = build_spec_fac()
+    for idx in df.index:
+        print(f"Working on subhalo {idx}")
+        stars = il.snapshot.loadSubhalo(sim_path, snap_num, idx, "stars")
+        utils.dropWindParticles(stars)
+        stars["Redshift"] = z
+        gas = il.snapshot.loadSubhalo(sim_path, snap_num, idx, "gas")
+
+        relevant_gas = get_star_forming_gas(gas)
+        get_particle_dist(relevant_gas, df, idx, z)
+        get_particle_dist(stars, df, idx, z)
+        idces_gas_in_rad = relevant_gas["rel_dist"] < 1
+        idces_gas_in_half_rad = relevant_gas["rel_dist"] < 0.5
+
+        idces_stars_in_rad = stars["rel_dist"] < 1
+        idces_stars_in_half_rad = stars["rel_dist"] < 0.5
+
+        gas_in_rad = get_subset(relevant_gas, idces_gas_in_rad)
+        gas_in_half_rad = get_subset(relevant_gas, idces_gas_in_half_rad)
+        stars_in_rad = get_subset(stars, idces_stars_in_rad)
+        stars_in_half_rad = get_subset(stars, idces_stars_in_half_rad)
+        get_radius_vol(gas_in_rad)
+        get_radius_vol(gas_in_half_rad)
+        relevant_stars_half_rad = select_stars_in_gas(
+            stars_in_half_rad, gas_in_half_rad
+        )
+        relevant_stars_rad = select_stars_in_gas(stars_in_rad, gas_in_rad)
+        # Reload the relevant stars
+        stars_in_rad = get_subset(stars, idces_stars_in_rad)
+        stars_in_half_rad = get_subset(stars, idces_stars_in_half_rad)
+        del stars
+
+        if stars_in_half_rad["count"] == 0:
+            df.drop(index=idx, inplace=True)
+            continue
+
+        stars_in_rad["ages"] = spectra._computeAgeFromFormationTime(
+            stars_in_rad["Redshift"], stars_in_rad["GFM_StellarFormationTime"]
+        )
+
+        stars_in_half_rad["ages"] = spectra._computeAgeFromFormationTime(
+            stars_in_half_rad["Redshift"], stars_in_half_rad["GFM_StellarFormationTime"]
+        )
+
+        if stars_in_rad["count"] < 10000:
+            tot_ion_em_rad, tot_bol_lum_rad = compute_small_gal_em(
+                stars_in_rad, specFac, idx
+            )
+            tot_ion_em_half_rad, tot_bol_lum_half_rad = compute_small_gal_em(
+                stars_in_half_rad, specFac, idx
+            )
+        else:
+            tot_ion_em_rad, tot_bol_lum_rad = compute_large_gal_em(
+                stars_in_rad, specFac, idx
+            )
+            tot_ion_em_half_rad, tot_bol_lum_half_rad = compute_large_gal_em(
+                stars_in_half_rad, specFac, idx
+            )
+
+        if relevant_stars_half_rad["count"] != 0:
+            relevant_stars_rad["ages"] = spectra._computeAgeFromFormationTime(
+                relevant_stars_rad["Redshift"],
+                relevant_stars_rad["GFM_StellarFormationTime"],
+            )
+
+            relevant_stars_half_rad["ages"] = spectra._computeAgeFromFormationTime(
+                relevant_stars_half_rad["Redshift"],
+                relevant_stars_half_rad["GFM_StellarFormationTime"],
+            )
+
+            if relevant_stars_rad["count"] < 10000:
+                ion_em_rad, bol_lum_rad = compute_small_gal_em(
+                    relevant_stars_rad, specFac, idx
+                )
+                ion_em_half_rad, bol_lum_half_rad = compute_small_gal_em(
+                    relevant_stars_half_rad, specFac, idx
+                )
+            else:
+                ion_em_rad, bol_lum_rad = compute_large_gal_em(
+                    relevant_stars_rad, specFac, idx
+                )
+                ion_em_half_rad, bol_lum_half_rad = compute_large_gal_em(
+                    relevant_stars_half_rad, specFac, idx
+                )
+
+        else:
+            ion_em_rad, bol_lum_rad = 0.0, 0.0
+            ion_em_half_rad, bol_lum_half_rad = 0.0, 0.0
+
+        bol_lums_2r.append(bol_lum_rad)
+        ion_ems_2r.append(ion_em_rad)
+        gas_masses_2r.append(np.sum(gas_in_rad["Masses"]))
+        star_masses_2r.append(np.sum(relevant_stars_rad["Masses"]))
+        sfrs_2r.append(np.sum(gas_in_rad["SFR"]))
+        metallicities_2r.append(
+            np.sum(gas_in_rad["Z"] * gas_in_rad["Masses"])
+            / np.sum(gas_in_rad["Masses"])
+        )
+        volumes_2r.append(np.sum(gas_in_rad["Volumes"]))
+
+        bol_lums_r.append(bol_lum_half_rad)
+        ion_ems_r.append(ion_em_half_rad)
+        gas_masses_r.append(np.sum(gas_in_half_rad["Masses"]))
+        star_masses_r.append(np.sum(relevant_stars_half_rad["Masses"]))
+        sfrs_r.append(np.sum(gas_in_half_rad["SFR"]))
+        metallicities_r.append(
+            np.sum(gas_in_half_rad["Z"] * gas_in_half_rad["Masses"])
+            / np.sum(gas_in_half_rad["Masses"])
+        )
+        volumes_r.append(np.sum(gas_in_half_rad["Volumes"]))
+
+        tot_bol_lums_2r.append(tot_bol_lum_rad)
+        tot_ion_ems_2r.append(tot_ion_em_rad)
+
+        tot_bol_lums_r.append(tot_bol_lum_half_rad)
+        tot_ion_ems_r.append(tot_ion_em_half_rad)
+
+    df["Bol_lum_sf_2r"] = np.array(bol_lums_2r) * L_sun_to_erg_s
+    df["Ion_em_sf_2r"] = np.array(ion_ems_2r)
+    df["M_gas_sf_2r"] = np.array(gas_masses_2r) * mass_to_g
+    df["SFR_sf_2r"] = np.array(sfrs_2r)
+    df["M_star_sf_2r"] = np.array(star_masses_2r) * mass_to_g
+    df["V_sf_2r"] = np.array(volumes_2r) * volume_to_cm3
+    df["Z_sf_2r"] = np.array(metallicities_2r)
+
+    df["Bol_lum_sf_r"] = np.array(bol_lums_r) * L_sun_to_erg_s
+    df["Ion_em_sf_r"] = np.array(ion_ems_r)
+    df["M_gas_sf_r"] = np.array(gas_masses_r) * mass_to_g
+    df["SFR_sf_r"] = np.array(sfrs_r)
+    df["M_star_sf_r"] = np.array(star_masses_r) * mass_to_g
+    df["V_sf_r"] = np.array(volumes_r) * volume_to_cm3
+    df["Z_sf_r"] = np.array(metallicities_r)
+
+    df["Tot_Bol_lum_2r"] = np.array(tot_bol_lums_2r) * L_sun_to_erg_s
+    df["Tot_Ion_em_2r"] = np.array(tot_ion_ems_2r)
+
+    df["Tot_Bol_lum_r"] = np.array(tot_bol_lums_r) * L_sun_to_erg_s
+    df["Tot_Ion_em_r"] = np.array(tot_ion_ems_r)
+    return
+
+
+if __name__ == "__main__":
+    level_1_name = "1_df.pickle"
+    snap_num = 13
+    level_2_name = "2_df.pickle"
+    base = "/ptmp/mpa/ivkos/semianalytic_fesc/sn013"
+
+    df_path = os.path.join(base, level_1_name)
+    df = pd.read_pickle(df_path)
+
+    snap_num = 13
+    sim, sim_path = get_sim()
+    z = get_redshift(sim, snap_num)
+    add_sf_quantities(df, sim_path, snap_num, z)
+
+    save_path = os.path.join(base, level_2_name)
+    df.to_pickle(save_path)
