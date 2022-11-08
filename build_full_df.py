4ed4a5a74f436c4e4d16cf1884519c756664ac2f
Ivan Kostyuk <kostyuk@hotmail.de>
2022-11-08 17:41:54 +0100

    Added docstring

--- build_full_df.py
+++ build_full_df.py
@@ -11,10 +11,29 @@ def merge_dfs(
     destination_path=None,
     name="full_df",
 ):
-    if destination_path is None:
-        destination_path = os.join(base, name + ".pickle")
+    """
+    Takes all individual dataframes for various snapshots and creates a
+    dataframe containing all data and an additional redshift column
 
-    for snap_num in range(snap_min, snap_max):
+    Parameters
+    ----------
+    snap_min : int
+        first snapshot number to add to the dataframe
+    snap_max : int
+        last snapshot number to add to the dataframe
+    base : str, optional
+        Path where the snapshot directories are stored, by default
+        "/ptmp/mpa/ivkos/semianalytic_fesc"
+    destination_path : str, optional
+        Path where to save the full df, by default it is the directory
+        containing the snapshot subdirectories, by default None
+    name : str, optional
+        Name of the final directory, by default "full_df"
+    """
+    if destination_path is None:
+        destination_path = os.path.join(base, name + ".pickle")
+
+    for snap_num in range(snap_min, snap_max + 1):
         df_name = "df_" + str(snap_num) + ".pickle"
         snap = get_snap(snap_num)
         sim, sim_path = get_sim()
@@ -25,11 +44,11 @@ def merge_dfs(
         if snap_num == snap_min:
             df_dict = {}
             for key in df.columns:
-                df_dict[key] = df[key].copy()
-            df_dict["z"] = np.ones(len(df)) * z
+                df_dict[key] = list(df[key].copy())
+            df_dict["z"] = list(np.ones(len(df)) * z)
         else:
             for key in df.columns:
-                df_dict[key].extend(df[key].copy)
+                df_dict[key].extend(list(df[key].copy()))
             df_dict["z"].extend(np.ones(len(df)) * z)
 
     full_df = pd.DataFrame.from_dict(df_dict)
