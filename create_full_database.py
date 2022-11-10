00cc400a21bb35b843394725f55c0d9a7c6578d6
Ivan Kostyuk <kostyuk@hotmail.de>
2022-11-10 21:44:55 +0100

    Changed to proper gridding

--- create_full_database.py
+++ create_full_database.py
@@ -1,43 +1,44 @@
 from build_initial_df_0 import build_new_df
 from get_height_and_maps_1 import update_df_height_make_maps
+from gridded_maps import grid_halos
 from calculate_gridded_fesc_2 import update_maps
 from maps_to_df_3 import update_map_df
 
 
 def create_database(
-    snap_nums,
+    num,
     df_name="df",
     maps_name="maps",
-    scale=1,
+    scale=100,
     base="/ptmp/mpa/ivkos/semianalytic_fesc",
 ):
-    for num in snap_nums:
-        df_name_full = df_name + "_" + str(num)
-        maps_name_full = maps_name + "_" + str(num)
-        build_new_df(snap_num=num, base=base)
-        update_df_height_make_maps(
-            snap_num=num,
-            df_name=df_name_full,
-            hdf_name=maps_name_full,
-            to_hdf=True,
-            adaptive=True,
-            avg_dist_weighting=1,
-        )
-        update_maps(
-            snap_num=num,
-            grid_size=scale,
-            hdf_name=maps_name_full,
-            df_name=df_name_full,
-        )
-        update_map_df(
-            snap_num=num,
-            scale=scale,
-            hdf_name=maps_name_full,
-            df_name=df_name_full,
-        )
+    df_name_full = df_name + "_" + str(num)
+    maps_name_full = maps_name + "_" + str(num)
+    build_new_df(snap_num=num, save_name=df_name_full, base=base)
+    grid_halos(
+        df_name=df_name_full,
+        snap_num=num,
+        grid_scale=scale,
+        hdf_name=maps_name_full,
+    )
+    # update_df_height_make_maps(
+    #     snap_num=num,
+    #     df_name=df_name_full,
+    #     hdf_name=maps_name_full,
+    #     to_hdf=True,
+    #     adaptive=True,
+    #     avg_dist_weighting=1,
+    # )
+    update_maps(
+        snap_num=num,
+        grid_size=scale,
+        hdf_name=maps_name_full,
+        df_name=df_name_full,
+    )
+    update_map_df(
+        snap_num=num,
+        scale=scale,
+        hdf_name=maps_name_full,
+        df_name=df_name_full,
+    )
     return
-
-
-if __name__ == "__main__":
-    snap_nums = [0]
-    create_database(snap_nums)
