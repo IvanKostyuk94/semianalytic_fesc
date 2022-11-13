281fd54421abddb4634a7785a6f0abfe7bf887d5
Ivan Kostyuk <kostyuk@hotmail.de>
2022-11-13 14:26:51 +0100

    Before creating a new database, check if gridded one already exists. Should only check for df in the future.

--- create_full_database.py
+++ create_full_database.py
@@ -3,6 +3,7 @@ from get_height_and_maps_1 import update_df_height_make_maps
 from gridded_maps import grid_halos
 from calculate_gridded_fesc_2 import update_maps
 from maps_to_df_3 import update_map_df
+from submit_runs import is_df_done
 
 
 def create_database(
@@ -14,7 +15,8 @@ def create_database(
 ):
     df_name_full = df_name + "_" + str(num)
     maps_name_full = maps_name + "_" + str(num)
-    build_new_df(snap_num=num, save_name=df_name_full, base=base)
+    if not is_df_done(df_name_full, num):
+        build_new_df(snap_num=num, save_name=df_name_full, base=base)
     grid_halos(
         df_name=df_name_full,
         snap_num=num,
